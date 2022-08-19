f7a2d82ad58f1b20a94bb245f11bc62a203c5a95
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-08-19 11:45:07 +0100

    Uploaded Unreal 5 project

---
+++ Source/MedievalEnvSound/MedievalEnvSound.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MedievalEnvSound : ModuleRules
+{
+	public MedievalEnvSound(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/MedievalEnvSound/MedievalEnvSoundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MedievalEnvSoundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMedievalEnvSoundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMedievalEnvSoundGameMode();
+};
+
+
+
